1902aadebd3b55be318a24ad30af09927dbcca84
Almogbcr <almogbcr@gmail.com>
2018-10-22 22:42:58 +0300

    Update BodyParser

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-var bodyPaser = require('body-parser');
+var bodyParser = require('body-parser');
 
 const PORT = 3000;
 
-app.use(bodyPaser.json());
-app.use(bodyPaser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
 
 app.listen(PORT, function (){
     console.log("Running on port " + PORT)

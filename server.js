d357cb4114daf6945b00ff3b8c2714b4d37954be
Almogbcr <almogbcr@gmail.com>
2018-10-22 22:49:44 +0300

    Port Change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-const PORT = 3001;
+const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));

30e10bb77d48250d6fb9c8a62bcf9d470900ad05
Almogbcr <almogbcr@gmail.com>
2018-10-23 20:06:09 +0300

    Change server.js to search.js

--- search.js
+++ search.js
@@ -1,35 +1,49 @@
 const fs = require('fs');
 const path = require('path');
 var dir = "C:\\MyFolder";
+var extention = process.argv[2];
+var word = process.argv[3];
+var found = false;
 
-//console.log("Usage : Node search [EXT] [TEXT]");
+//File name
+var fullFileName = path.basename(process.argv[1]);
+var mExtention = path.extname(fullFileName);
+var mFileName = path.basename(fullFileName,mExtention);
+//Service Name
+var fullServiceName = path.basename(process.argv[0]);
+var serviceExt = path.extname(fullServiceName);
+var serviceName = path.basename(fullServiceName,serviceExt);
 
-function fileSearch(startPath, filter) {
+function getHelp(){
+    console.log("Usage: " + serviceName + " " + mFileName + " [EXT] [TEXT]");
+}
+
+function fileSearch(startPath, filter , word) {
     //Check if Folder exists
     if(!fs.existsSync(startPath)) {
-        console.log("Folder not found " + startPath);
         console.log("Creating Dir");
         fs.mkdirSync(startPath);
         console.log("Folder Created");
-    }else if (fs.existsSync(startPath)){
-        //console.log("Folder already exists");
     }
-
     var files = fs.readdirSync(startPath);
-
     for (var i = 0; i < files.length ; i++) {
         var fileName = path.join(startPath,files[i]);
         var stat = fs.lstatSync(fileName);
         if (stat.isDirectory()){
-            fileSearch(fileName,filter);
-        }
-        else if (fileName.indexOf(filter) >= 0){
-            console.log(fileName);
-        }
-
+            found = found || fileSearch(fileName,filter,word);
+        } else {
+                fs.readFile(fileName, 'utf8', function (err, data) {
+                    if (err) throw err;
+                    if(data.localeCompare(word) >= 0){
+                        console.log(fileName);
+                        found = true;
+                    }
+                });
+            }
     }
+    return found;
+
 }
-
-
-fileSearch(dir, '.txt');
-
+if(!fileSearch(dir , extention , word)){
+    
+}

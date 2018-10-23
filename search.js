35235ac5441e42ad1b1b96437aa94a08a17bff03
Almogbcr <almogbcr@gmail.com>
2018-10-23 12:11:47 +0300

    Change server.js to search.js

--- search.js
+++ search.js
@@ -1,21 +1,36 @@
 const fs = require('fs');
+const path = require('path');
+var dir = "C:\\MyFolder";
+var word = "Ori";
 
-var extension = ".txt";
-var text = "ori";
+//console.log("Usage : Node search [EXT] [TEXT]");
 
+function fileSearch(startPath,filter,word) {
+    //Check if Folder exists
+    if(!fs.existsSync(startPath)) {
+        console.log("Folder not found " + startPath);
+        console.log("Creating Dir");
+        fs.mkdirSync(startPath);
+        console.log("Folder Created");
+    }else if (fs.existsSync(startPath)){
+        //console.log("Folder already exists");
+    }
 
+    var files = fs.readdirSync(startPath);
 
-function helpMessage (){
-    console.log("Usage node search [EXT] [TEXT]");
+    for (var i = 0; i < files.length ; i++) {
+        var fileName = path.join(startPath,files[i]);
+        var stat = fs.lstatSync(fileName);
+        if (stat.isDirectory()){
+            fileSearch(fileName,filter);
+        }
+        else if (fileName.indexOf(filter) >= 0){
+            console.log(fileName);
+        }
+
+    }
 }
 
-function lookInTxtFiles(){
-        fs.readFile('almog.txt' , 'utf8' , function (err , data) {
-            if (err) throw  err;
-            console.log(data)
-        })
-}
 
-lookInTxtFiles();
-
+//fileSearch(dir,'.txt',word);
 

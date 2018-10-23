cac1cc293efe9074190108d9b0d80fcc2ffb4455
Almogbcr <almogbcr@gmail.com>
2018-10-23 20:28:18 +0300

    Update Comments

--- search.js
+++ search.js
@@ -1,6 +1,9 @@
+//Using Node Modules
 const fs = require('fs');
 const path = require('path');
+//Define first Dir
 var dir = "C:\\MyFolder";
+//Define Arguments for CMD NodeJS
 var extention = process.argv[2];
 var word = process.argv[3];
 
@@ -44,14 +47,14 @@ function fileSearch(firstDir, filter, word) {
             //What happens if found something that is not directory
         } else {
             //Reading from the File
-                fs.readFile(fileName, 'utf8', function (err, data) {
-                    if (err) throw err;
-                    //Printing the files that contains the defined word
-                    if(data.localeCompare(word) >= 0){
-                        console.log(fileName);
-                    }
-                });
-            }
+            fs.readFile(fileName, 'utf8', function (err, data) {
+                if (err) throw err;
+                //Printing the files that contains the defined word
+                if(data.localeCompare(word) >= 0){
+                    console.log(fileName);
+                }
+            });
+        }
     }
 
 }
